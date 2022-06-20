65dfd628b799a0ab4d01c5e7c1c3886278f8bf9c
Renan Arimatea <renanmoraiz@gmail.com>
2022-06-20 11:32:47 -0300

    background teste

--- src/components/pages/HomeStyled.js
+++ src/components/pages/HomeStyled.js
@@ -18,7 +18,7 @@ min-height: 100vh;
 
 @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
     background-color: #000;
-    background-image:url(${process.env.PUBLIC_URL + 'https://images.unsplash.com/photo-1476136236990-838240be4859?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'});
+    background-image:url(${process.env.PUBLIC_URL + 'https://images.unsplash.com/photo-1593769645155-d6416081c0c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=682&q=80'});
     background-position: center;
     background-attachment: fixed;
     background-repeat: no-repeat;
